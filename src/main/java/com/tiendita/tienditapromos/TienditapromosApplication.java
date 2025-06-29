d1268ee0a84bf69bd2e1d0d8247aab4913ac12e2
sebastianflesa <sebastian.orrego@fleischmann.cl>
2025-06-29 19:35:44 -0400

    Initial commit

---
+++ src/main/java/com/tiendita/tienditapromos/TienditapromosApplication.java
@@ -0,0 +1,13 @@
+package com.tiendita.tienditapromos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TienditapromosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TienditapromosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendita/tienditapromos/TienditapromosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendita.tienditapromos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TienditapromosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
